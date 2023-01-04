499b98d6ea351f2f5e718f62a0c0fd6931bc14bb
jashs13 <jashjshah13@gmail.com>
2023-01-04 16:10:14 +0530

    Prob 876

---
+++ MidOfLL876.java
@@ -0,0 +1,61 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int cnt = 0;
+        List<ListNode> arrlist = new ArrayList<ListNode>();
+        while(head!=null){
+            cnt += 1;
+            arrlist.add(head);
+            head = head.next;
+        }
+        //System.out.println("Count : "+ cnt);
+        //System.out.println("ArrayList = " + arrlist);
+        //for(int i = cnt/2; i<cnt; i++){
+        //    arr = arrlist.toArray(arr);
+        //}
+
+        return arrlist.get(cnt/2);
+    }
+}
+
+/* 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int len=0;
+
+        ListNode temp = head;
+        //Calculating length of the linked list
+        while(temp!=null){
+            temp = temp.next;
+            len++;
+        }
+        //finding middle of linked list
+        len = len/2;
+        ListNode currentNode = head;
+        // iterate till length of linked list becomes 0
+        while(len>0){
+            currentNode = currentNode.next; //shifting head forward
+            len--; //reducing length
+        }
+        return currentNode; //node at which length becomes 0, is middle node
+    }
+}
+ */
+
+/*
+class Solution {
+    public ListNode middleNode(ListNode head) {
+      ListNode slow=head;
+      ListNode fast=head;
+        
+      while(fast.next!=null && fast.next.next!=null){
+          slow=slow.next;
+          fast=fast.next.next;
+      }
+      
+      if(fast.next!=null){
+          slow=slow.next;
+      }
+      return slow;  
+    }
+}
+*/ 
